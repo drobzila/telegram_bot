f74503d8df349693e8b0e68bd52cd1b86c363606
Azri <lesquatrefreresazri@gmail.com>
2026-09-03 22:20:57 +0100

    Consolidate duplicate YouTube token storage

--- database/youtube_tokens.py
+++ database/youtube_tokens.py
@@ -1,35 +1,5 @@
-from database.db import get_connection
+"""Backward-compatible wrapper for the canonical YouTube OAuth token store."""
 
+from database.oauth_tokens import delete_token, get_token, save_token
 
-def save_token(telegram_id, access_token, refresh_token, expires_at):
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                INSERT INTO oauth_tokens (
-                    telegram_id,
-                    access_token,
-                    refresh_token,
-                    expires_at
-                )
-                VALUES (%s, %s, %s, %s)
-                ON CONFLICT (telegram_id)
-                DO UPDATE SET
-                    access_token = EXCLUDED.access_token,
-                    refresh_token = EXCLUDED.refresh_token,
-                    expires_at = EXCLUDED.expires_at
-            """, (
-                telegram_id,
-                access_token,
-                refresh_token,
-                expires_at,
-            ))
-
-def get_token(telegram_id):
-    with get_connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("""
-                SELECT *
-                FROM oauth_tokens
-                WHERE telegram_id = %s
-            """, (telegram_id,))
-            return cur.fetchone()
+__all__ = ["save_token", "get_token", "delete_token"]
